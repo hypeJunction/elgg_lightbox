716678986a4f6b89fb9d3a5f71819f0923771f61
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-10-05 18:23:41 +0300

    fix(lightbox): set default settings before attempting to read those set by php

--- views/default/js/lightbox/settings.js
+++ views/default/js/lightbox/settings.js
@@ -1,6 +1,17 @@
 define(['elgg', 'jquery'], function (elgg, $) {
 	elgg.provide('elgg.ui.lightbox');
-	var settings = {};
+	var settings = {
+		current: elgg.echo('js:lightbox:current', ['{current}', '{total}']),
+		previous: elgg.echo('previous'),
+		next: elgg.echo('next'),
+		close: elgg.echo('close'),
+		xhrError: elgg.echo('error:default'),
+		imgError: elgg.echo('error:default'),
+		opacity: 0.5,
+		maxWidth: '100%',
+		// don't move colorbox on small viewports https://github.com/Elgg/Elgg/issues/5312
+		reposition: $(window).height() > 600
+	};
 	if (typeof elgg.ui.lightbox.getSettings === 'function') {
 		settings = elgg.ui.lightbox.getSettings();
 	}
