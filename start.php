9d9aac9fbcd1c3167340b3e1b07da56e5d505c97
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-02-23 09:05:14 +0100

    feat(css): replace core theme view and integrate with mrclay_combiner

--- start.php
+++ start.php
@@ -6,7 +6,6 @@
  * @author Ismayil Khayredinov <info@hypejunction.com>
  * @copyright Copyright (c) 2015, Ismayil Khayredinov
  */
-
 elgg_register_event_handler('init', 'system', 'elgg_lightbox_init');
 
 /**
@@ -15,12 +14,14 @@ elgg_register_event_handler('init', 'system', 'elgg_lightbox_init');
  */
 function elgg_lightbox_init() {
 
-	elgg_unregister_js('lightbox');
-	elgg_extend_view('elgg.js', 'lightbox.js');
-	
-	elgg_unregister_css('lightbox');
-	elgg_extend_view('elgg.css', 'elgg/lightbox.css');
-	elgg_extend_view('admin.css', 'elgg/lightbox.css');
+	if (!elgg_is_active_plugin('mrclay_combiner')) {
+		elgg_unregister_js('lightbox');
+		elgg_extend_view('elgg.js', 'lightbox.js');
+
+		elgg_unregister_css('lightbox');
+		elgg_extend_view('elgg.css', 'elgg/lightbox.css');
+		elgg_extend_view('admin.css', 'elgg/lightbox.css');
+	}
 
 	elgg_extend_view('theme_sandbox/javascript', 'theme_sandbox/javascript/lightbox-photo');
 	elgg_extend_view('theme_sandbox/javascript', 'theme_sandbox/javascript/lightbox-iframe');
---
+++ views.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+	'default' => array(
+		'lightbox/elgg-colorbox-theme/colorbox.css' => __DIR__ . '/views/default/elgg/lightbox.css',
+	)
+);
